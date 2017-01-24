9863767f1283e48271db3d08273bebb3d4fdf478
jmlopezt <juanto1514@gmail.com>
2017-01-24 12:07:03 +0100

    Added two last group session
    Image decoded & Doppler effect deleted

--- Practica3/decop3NOAA.m
+++ Practica3/decop3NOAA.m
@@ -2,8 +2,12 @@
 % Recepción Analógica empleando el estándard APT.
 %
 %  Autores -> Manuel Moya Ferrer
-%             Jose Manuel Garcia Gimenez
+%             Jose Manuel Garcia Gimeno
 %             Juan Manuel Lopez Torralba
+
+clear all
+close all
+clc
 
 
 % Leemos el fichero de origen y cargamos los datos en la variable x
@@ -16,8 +20,7 @@ FsAM = 11025;
 Fs = 60e3;
 FsFinal = 4160;
 fc = 2.4e3;
-
-t = 1/Fs:1/Fs:(length(x)-1)/Fs;
+%t = 1/Fs:1/Fs:(length(x)-1)/Fs;
 
 % Obtenemos la señal en fase y cuadratura del archivo de entrada.
 x_I = x(1:2:end);
@@ -25,25 +28,57 @@ x_Q = x(2:2:end);
 
 x_FM = x_I + 1j*x_Q;
 
-
 % Demodulameos la señal FM
-
-x_demod_FM=angle(x(2:end).*conj(x(1:end-1)));   % señal demodulada
-% figure;plot(t,x_demod);
+x_demod_FM=angle(x_FM(2:end).*conj(x_FM(1:end-1)));   % señal demodulada
+x_demod_FM=resample(x_demod_FM,11025,60000);
 
 % Demodulamos AM
-
-x_demod_AM = amdemod(resample(x_demod_FM,11025,60000),fc,FsAM);
-% figure; plot(x_demod_AM);
+x_demod_AM = abs(x_demod_FM);
 
 m = resample(x_demod_AM,4160,11025);
-figure; plot(m)
 
-fftx = fft(x_demod_FM);
+figure
+plot(m)
 
-f=linspace(0,Fs/2,length(x_demod_FM)/2-1)/1000;
-figure;
-plot(f,abs(fftx(1:length(fftx)/2-1)));
+%% OPCION FACIL
+
+imageData=m(51097:1974058-1082);
+
+for i=0:1:900
+    vectorImag(i+1,:)=imageData(1+2080*i:2080+2080*i);
+end
+
+
+
+%% OPCION GOSELIANA
+
+a=[1 1 0 0];
+pulsoA=[zeros(1,4) a a a a a a a zeros(1,8)]; % pulso para el visible
+corr_PulsoA=xcorr(m,pulsoA);
+corr_PulsoA_shifted=circshift(corr_PulsoA,(length(corr_PulsoA)-1)/2 );
+[picos,posiciones_picos]=findpeaks(corr_PulsoA_shifted,'MINPEAKHEIGHT',13,'MINPEAKDISTANCE',1900);
+
+
+
+for i=1:1:length(posiciones_picos) -1
+        vectorImag2(i,:)=resample(m(posiciones_picos(i):posiciones_picos(i+1)),2080,length(m(posiciones_picos(i):posiciones_picos(i+1))));  
+end
+
+subplot(2,1,1)
+imshow(vectorImag)
+title('Imagen sacada a ojo')
+subplot(2,1,2)
+imshow(vectorImag2)
+title('Imagen goseliana')
+
+
+figure
+imshow(vectorImag2)
+
+
+
+
+
 
 
 
